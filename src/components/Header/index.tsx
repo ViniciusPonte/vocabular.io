c387eabe161195ea318b32fcb077e4453ed2718d
Vinicius Ponte <viniciusbponte@gmail.com>
2022-02-23 20:20:46 -0300

    header changed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ export function Header(){
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <h3>TERM.OOO</h3>
+                <span>V<span style={{color: 'yellow'}}>O</span>CA<span style={{color: 'red'}}>B</span>ULA<span style={{color: 'green'}}>R</span>.IO</span>
             </div>
         </header>
     );

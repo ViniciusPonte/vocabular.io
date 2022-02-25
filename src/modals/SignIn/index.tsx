d09057721fce22e17381123b792535efa7c64cec
Vinicius Ponte <viniciusbponte@gmail.com>
2022-02-25 15:32:02 -0300

    button signinwithgithub

--- src/modals/SignIn/index.tsx
+++ src/modals/SignIn/index.tsx
@@ -2,10 +2,12 @@ import { FaGithub, FaFacebook, FaGoogle } from 'react-icons/fa'
 import styles from './styles.module.scss';
 import { useState } from 'react';
 import { signIn } from "next-auth/react"
+import { useAuth } from '../../contexts/auth';
 
 export function SignIn(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const { signInWithGithub } = useAuth();
 
     return (
         <div className={styles.container}>
@@ -19,7 +21,7 @@ export function SignIn(){
             <div className={styles.socialButtons}>
                 <button style={{backgroundColor: '#3A63BE'}}><FaFacebook size={20}/></button>
                 <button style={{backgroundColor: '#C94130'}}><FaGoogle size={20}/></button>
-                <button style={{backgroundColor: '#161B22'}}><FaGithub size={20}/></button>
+                <button style={{backgroundColor: '#161B22'}} onClick={() => signInWithGithub()}><FaGithub size={20}/></button>
             </div>
         </div>
     )
